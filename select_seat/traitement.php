3960542d2870cbdfef56c10f9017b38af5fb063a
Khodabuccus matthieu <matthieu.khodabuccus1@gmail.com>
2020-09-21 11:57:58 +0200

    selection siège

---
+++ select_seat/traitement.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Traitement des données siège</title>
+</head>
+<body>
+<?
+// on teste la déclaration de nos variables
+if (isset($_POST['']) && isset($_POST[''])) {
+	// on affiche nos résultats
+	echo 'Votre place est '.$_POST[''].';
+}
+?>
+</body>
+</html>
